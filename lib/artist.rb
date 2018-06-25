2693418b42a6ad111cafbd26736783e8a1db0920
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-25 19:00:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,7 @@ class Artist
   @@artists = []
 
   def initialize
-    @@artists << self
+    self.class.all << self
     @songs = []
   end
 
@@ -28,9 +28,5 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-#  def to_param
-#    name.downcase.gsub(' ', '-')
-# end
 
 end
